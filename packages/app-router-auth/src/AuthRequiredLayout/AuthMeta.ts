5f9b7258565e9bc12a08fd1c1ec633ccab41e6a7
^_^肥仔John <fsjohnhuang@hotmail.com>
2023-11-09 10:41:47 +0800

    feat(app-router-auth): add makeAuthorize to simplify the defining of authorize

--- packages/app-router-auth/src/AuthRequiredLayout/AuthMeta.ts
+++ packages/app-router-auth/src/AuthRequiredLayout/AuthMeta.ts
@@ -6,4 +6,20 @@ interface AuthMeta {
   authorize?: Authorize;
 }
 
-export type { Authorize, AuthMeta };
+interface MakeAuthorizeArg<P> {
+  authorize(path: string, permissions: P): boolean;
+  getPermissions(): P | Promise<P>;
+}
+
+const makeAuthorize =
+  <P>({ authorize, getPermissions }: MakeAuthorizeArg<P>) =>
+  (path: string) => {
+    const permissions = getPermissions();
+    if (permissions instanceof Promise) {
+      return permissions.then((permissions) => authorize(path, permissions));
+    }
+    return authorize(path, permissions);
+  };
+
+export type { Authorize, AuthMeta, MakeAuthorizeArg };
+export { makeAuthorize };
--- packages/app-router-auth/src/index.ts
+++ packages/app-router-auth/src/index.ts
@@ -1,6 +1,7 @@
 export type { AuthContextType, AuthProviderProps } from "./AuthContext/AuthContext";
 export { useAuth, AuthProvider } from "./AuthContext/AuthContext";
-export type { AuthMeta } from "./AuthRequiredLayout/AuthMeta";
+export type { AuthMeta, MakeAuthorizeArg } from "./AuthRequiredLayout/AuthMeta";
+export { makeAuthorize } from "./AuthRequiredLayout/AuthMeta";
 export type { SealedAuthMeta } from "./AuthRequiredLayout/sealAuthMeta";
 export { default as sealAuthMeta } from "./AuthRequiredLayout/sealAuthMeta";
 export type { AuthRequiredLayoutProps } from "./AuthRequiredLayout/AuthRequiredLayout";
