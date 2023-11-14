1a3119da92f1303190ecfb28232d0b91661639eb
^_^肥仔John <fsjohnhuang@hotmail.com>
2023-11-14 15:50:16 +0800

    feat(app-router-cli): add watch mode

--- packages/app-router-cli/cli.js
+++ packages/app-router-cli/cli.js
@@ -1,11 +1,21 @@
 import { writeFileSync } from "node:fs";
 import { join } from "node:path";
+import { debounce } from "lodash-es";
+import { watch } from "chokidar"; // https://github.com/paulmillr/chokidar
+import { format } from "prettier"; // https://prettier.io/docs/en/api.html
 import { getRoutesFromFileSystem, generateRouterOutput } from "./src/index.js";
-import { format } from "prettier";
 
 const sourcePath = join(process.cwd(), "./logs/app");
 const outputPath = join(process.cwd(), "./logs/router.tsx");
 
-const output = generateRouterOutput(getRoutesFromFileSystem(outputPath, sourcePath));
+const handleChange = debounce(async () => {
+  console.log("Generating app router...");
+  const output = generateRouterOutput(getRoutesFromFileSystem(outputPath, sourcePath));
+  writeFileSync(outputPath, await format(output, { parser: "babel" }));
+  console.log("Generating app router is done.");
+}, 500);
 
-writeFileSync(outputPath, await format(output, { parser: "babel" }));
+watch(sourcePath).on("add", handleChange);
+watch(sourcePath).on("addDir", handleChange);
+watch(sourcePath).on("unlink", handleChange);
+watch(sourcePath).on("unlinkDir", handleChange);
