96fc7626b845c0351203ef99489fd8afd006c69d
^_^肥仔John <fsjohnhuang@hotmail.com>
2023-11-18 08:12:58 +0800

    fix(app-router): setup with initial function

--- packages/app-router/src/utils/useEvent.ts
+++ packages/app-router/src/utils/useEvent.ts
@@ -2,12 +2,14 @@ import { useRef, useLayoutEffect, useCallback } from "react";
 
 //eslint-disable-next-line @typescript-eslint/no-explicit-any
 const useEvent = <F extends (...args: any[]) => any>(handler: F) => {
-  const ref = useRef<F>();
+  const ref = useRef<F>(handler);
   useLayoutEffect(() => {
     ref.current = handler;
   });
 
-  return useCallback((...args: any[]) => ref.current?.call(null, ...args), []);
+  return useCallback((...args: any[]) => {
+    return ref.current.call(null, ...args);
+  }, []);
 };
 
 export default useEvent;
