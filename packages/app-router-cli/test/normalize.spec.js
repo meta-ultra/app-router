959b686f3d1c30e08dc18b659e12f7d43fc19de7
^_^肥仔John <fsjohnhuang@hotmail.com>
2024-01-05 17:01:20 +0800

    test(app-router-cli): rename data to app1

--- packages/app-router-cli/test/normalize.spec.js
+++ packages/app-router-cli/test/normalize.spec.js
@@ -1,114 +1,114 @@
 import normalize from "../src/core/normalize";
 import { cloneDeep } from "lodash";
-import data from "./data";
+import app1 from "./app1";
 
-let output = undefined;
+let output1 = undefined;
 beforeEach(() => {
-  output = cloneDeep(data);
+  output1 = cloneDeep(app1);
 });
 
 test("set error of the root route to its global-error if exists", () => {
-  normalize(output);
+  normalize(output1);
 
-  expect(!!data[0].props.error).toBe(true);
-  expect(!output[0].props["global-error"]).toBe(true);
-  expect(!!output[0].props.error).toBe(true);
-  const error = output[0].props.error.split("/");
+  expect(!!app1[0].props.error).toBe(true);
+  expect(!output1[0].props["global-error"]).toBe(true);
+  expect(!!output1[0].props.error).toBe(true);
+  const error = output1[0].props.error.split("/");
   expect(error[error.length - 1]).toStrictEqual("global-error.js");
 });
 
 test("remove global-error property from the nested routes", () => {
-  normalize(output);
+  normalize(output1);
 
-  let node = data[0].children.find((child) => child.path === "app/about");
+  let node = app1[0].children.find((child) => child.path === "app1/about");
   expect(!!node.props["global-error"]).toBe(true);
-  node = output[0].children.find((child) => child.path === "app/about");
+  node = output1[0].children.find((child) => child.path === "app1/about");
   expect(node.props["global-error"]).toBe(undefined);
 });
 
 test("fulfill a default root layout if it dosen't exist", () => {
-  normalize(output);
-  expect(output[0].props.layout).toStrictEqual("app/layout.tsx");
+  normalize(output1);
+  expect(output1[0].props.layout).toStrictEqual("app1/layout.tsx");
 
-  const withoutRootLayout = cloneDeep(data);
+  const withoutRootLayout = cloneDeep(app1);
   delete withoutRootLayout[0].props.layout;
   normalize(withoutRootLayout);
   expect(withoutRootLayout[0].props.layout).toStrictEqual("preset::root-layout");
 });
 
 test("fulfill a default layout if there is a loading, not-found or error but no layout inside a nested route", () => {
-  normalize(output);
+  normalize(output1);
 
-  let aboutNode = data[0].children.find((node) => node.path === "app/about");
-  let nestedNode = aboutNode.children.find((node) => node.path === "app/about/nested");
+  let aboutNode = app1[0].children.find((node) => node.path === "app1/about");
+  let nestedNode = aboutNode.children.find((node) => node.path === "app1/about/nested");
   expect(nestedNode.props.layout).toBe(undefined);
 
-  aboutNode = output[0].children.find((node) => node.path === "app/about");
-  nestedNode = aboutNode.children.find((node) => node.path === "app/about/nested");
+  aboutNode = output1[0].children.find((node) => node.path === "app1/about");
+  nestedNode = aboutNode.children.find((node) => node.path === "app1/about/nested");
   expect(nestedNode.props.layout).toStrictEqual("preset::layout");
 });
 
 test("remain routes with neither `props.page` nor `props.layout`, but its descendants have", () => {
-  normalize(output);
+  normalize(output1);
 
-  expect(data[0].children.find((node) => node.path === "app/not-empty")).toBeDefined();
-  expect(output[0].children.find((node) => node.path === "app/not-empty")).toBeDefined();
+  expect(app1[0].children.find((node) => node.path === "app1/not-empty")).toBeDefined();
+  expect(output1[0].children.find((node) => node.path === "app1/not-empty")).toBeDefined();
 });
 
 test("remove routes without `props.page` and `props.layout` and its descendants are with neither `props.page` nor `props.layout`", () => {
-  normalize(output);
+  normalize(output1);
 
-  expect(data[0].children.find((node) => node.path === "app/empty")).toBeDefined();
-  expect(output[0].children.find((node) => node.path === "app/empty")).toBeUndefined();
+  expect(app1[0].children.find((node) => node.path === "app1/empty")).toBeDefined();
+  expect(output1[0].children.find((node) => node.path === "app1/empty")).toBeUndefined();
 });
 
 test("rename not-found to notFound", () => {
-  normalize(output);
+  normalize(output1);
 
-  let aboutNode = data[0].children.find((node) => node.path === "app/about");
-  let nestedNode = aboutNode.children.find((node) => node.path === "app/about/nested");
+  let aboutNode = app1[0].children.find((node) => node.path === "app1/about");
+  let nestedNode = aboutNode.children.find((node) => node.path === "app1/about/nested");
   expect(nestedNode.props["not-found"]).toBeDefined();
 
-  aboutNode = output[0].children.find((node) => node.path === "app/about");
-  nestedNode = aboutNode.children.find((node) => node.path === "app/about/nested");
+  aboutNode = output1[0].children.find((node) => node.path === "app1/about");
+  nestedNode = aboutNode.children.find((node) => node.path === "app1/about/nested");
   expect(nestedNode.props["not-found"]).toBeUndefined();
   expect(nestedNode.props["notFound"]).toBeDefined();
 });
 
 test("remove nested routes inside catch-all routes or optional catch-all routes", () => {
-  normalize(output);
+  normalize(output1);
 
-  let catchAllNode = data[0].children.find((node) => node.path === "app/catch-all");
+  let catchAllNode = app1[0].children.find((node) => node.path === "app1/catch-all");
   expect(
-    catchAllNode.children.find((node) => node.path === "app/catch-all/[[...id]]").children
+    catchAllNode.children.find((node) => node.path === "app1/catch-all/[[...id]]").children
   ).toHaveLength(1);
   expect(
-    catchAllNode.children.find((node) => node.path === "app/catch-all/[...id]").children
+    catchAllNode.children.find((node) => node.path === "app1/catch-all/[...id]").children
   ).toHaveLength(1);
   expect(
-    catchAllNode.children.find((node) => node.path === "app/catch-all/[id]").children
+    catchAllNode.children.find((node) => node.path === "app1/catch-all/[id]").children
   ).toHaveLength(1);
 
-  catchAllNode = output[0].children.find((node) => node.path === "app/catch-all");
+  catchAllNode = output1[0].children.find((node) => node.path === "app1/catch-all");
   expect(
-    catchAllNode.children.find((node) => node.path === "app/catch-all/[[...id]]").children
+    catchAllNode.children.find((node) => node.path === "app1/catch-all/[[...id]]").children
   ).toHaveLength(0);
   expect(
-    catchAllNode.children.find((node) => node.path === "app/catch-all/[...id]").children
+    catchAllNode.children.find((node) => node.path === "app1/catch-all/[...id]").children
   ).toHaveLength(0);
   expect(
-    catchAllNode.children.find((node) => node.path === "app/catch-all/[id]").children
+    catchAllNode.children.find((node) => node.path === "app1/catch-all/[id]").children
   ).toHaveLength(1);
 });
 
 test("sink the page to the level below if there's layout along with", () => {
-  normalize(output);
+  normalize(output1);
 
-  let homeNode = data[0].children.find((node) => node.path === "app/home");
+  let homeNode = app1[0].children.find((node) => node.path === "app1/home");
   expect(homeNode.props.page).toBeDefined();
   expect(homeNode.children).toHaveLength(0);
 
-  homeNode = output[0].children.find((node) => node.path === "app/home");
+  homeNode = output1[0].children.find((node) => node.path === "app1/home");
   expect(homeNode.props.page).toBeUndefined();
   expect(homeNode.children).toHaveLength(1);
   expect(homeNode.children[0].path).toStrictEqual(`${homeNode.path}/`);
--- packages/app-router-cli/test/traverseFileSystem.spec.js
+++ packages/app-router-cli/test/traverseFileSystem.spec.js
@@ -1,9 +1,9 @@
 import { join } from "node:path";
 import traverseFileSystem from "../src/core/traverseFileSystem";
-import data from "./data";
+import app1 from "./app1";
 
 test("no private items", () => {
-  const output = traverseFileSystem(join(__dirname, "./src"), join(__dirname, "./src/app"));
+  const output1 = traverseFileSystem(join(__dirname, "./src"), join(__dirname, "./src/app1"));
 
   const containsPrivateItems = (nodes, contains = false) => {
     for (let i = 0; !contains && i < nodes.length; ++i) {
@@ -22,18 +22,18 @@ test("no private items", () => {
     return contains;
   };
 
-  expect(containsPrivateItems(output)).toBe(false);
+  expect(containsPrivateItems(output1)).toBe(false);
 });
 
 test("select the first one sorted alphabetically when there're duplicated files with the same name.", () => {
-  const output = traverseFileSystem(join(__dirname, "./src"), join(__dirname, "./src/app"));
+  const output1 = traverseFileSystem(join(__dirname, "./src"), join(__dirname, "./src/app1"));
 
-  const homeNode = output[0].children.find((node) => node.path === "app/home");
+  const homeNode = output1[0].children.find((node) => node.path === "app1/home");
 
   expect(homeNode.props["error"].split(".")[1]).toBe("jsx");
 });
 
-describe("the full paths for page, layout and others are relative to the `outputPath` parameter", () => {
+describe("the full paths for page, layout and others are relative to the `output1Path` parameter", () => {
   const startsWith = (nodes, prefix, success = true) => {
     for (let i = 0; success && i < nodes.length; ++i) {
       const node = nodes[i];
@@ -51,32 +51,35 @@ describe("the full paths for page, layout and others are relative to the `output
     return success;
   };
 
-  test("starts with src/app/", () => {
-    const output = traverseFileSystem(join(__dirname, "./"), join(__dirname, "./src/app"));
-    expect(startsWith(output, "src/app/")).toBe(true);
+  test("starts with src/app1/", () => {
+    const output1 = traverseFileSystem(join(__dirname, "./"), join(__dirname, "./src/app1"));
+    expect(startsWith(output1, "src/app1/")).toBe(true);
   });
 
-  test("starts with app/", () => {
-    const output = traverseFileSystem(join(__dirname, "./src"), join(__dirname, "./src/app"));
-    expect(startsWith(output, "app/")).toBe(true);
+  test("starts with app1/", () => {
+    const output1 = traverseFileSystem(join(__dirname, "./src"), join(__dirname, "./src/app1"));
+    expect(startsWith(output1, "app1/")).toBe(true);
   });
 
-  test("starts with ../app/", () => {
-    const output = traverseFileSystem(join(__dirname, "./src/test"), join(__dirname, "./src/app"));
-    expect(startsWith(output, "../app/")).toBe(true);
-  });
-
-  test("starts with ../../app/", () => {
-    const output = traverseFileSystem(
-      join(__dirname, "./src/test/test"),
-      join(__dirname, "./src/app")
+  test("starts with ../app1/", () => {
+    const output1 = traverseFileSystem(
+      join(__dirname, "./src/test"),
+      join(__dirname, "./src/app1")
     );
-    expect(startsWith(output, "../../app/")).toBe(true);
+    expect(startsWith(output1, "../app1/")).toBe(true);
+  });
+
+  test("starts with ../../app1/", () => {
+    const output1 = traverseFileSystem(
+      join(__dirname, "./src/test/test"),
+      join(__dirname, "./src/app1")
+    );
+    expect(startsWith(output1, "../../app1/")).toBe(true);
   });
 });
 
 test("test the generated folder tree fully", () => {
-  const output = traverseFileSystem(join(__dirname, "./src"), join(__dirname, "./src/app"));
+  const output1 = traverseFileSystem(join(__dirname, "./src"), join(__dirname, "./src/app1"));
 
-  expect(output).toStrictEqual(data);
+  expect(output1).toStrictEqual(app1);
 });
