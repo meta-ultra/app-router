740b42f0011eefb0c414a167404c7f71ba6a78d0
^_^肥仔John <fsjohnhuang@hotmail.com>
2024-02-08 11:16:49 +0800

    fix(cli): uppercase routes are ignored

--- packages/app-router-cli/src/core/constants.js
+++ packages/app-router-cli/src/core/constants.js
@@ -5,16 +5,16 @@ const GLOBAL_ERROR = "global-error";
 const NOT_FOUND = "not-found";
 
 const INDEX_RE = /^\s*$/; // index page route
-const NORMAL_RE = /^[a-z][a-z0-9_-]*$/; // normal route
-const GROUP_RE = /^\(([.]{3})?[a-z][a-z0-9-_]*\)$/;
-const DYNAMIC_RE = /^\[([a-z][a-z0-9_]*)\]$/; // dynamic route
-const CATCH_ALL_RE = /^\[\.{3}([a-z][a-z0-9_]*)\]$/; // catch-all route
-const OPTIONAL_CATCH_ALL_RE = /^\[\[\.{3}([a-z][a-z0-9_]*)\]\]$/; // optional catch-all route
-const PARALLEL_RE = /^@([a-z][a-z0-9-_]*)$/; // parallel route
-const INTERCEPTING_SAME_LEVEL_RE = /^\(\.\)([a-z][a-z0-9-_]*)$/; // intercepting route for the same level
-const INTERCEPTING_ONE_LEVEL_UP_RE = /^\(\.\.\)([a-z][a-z0-9-_]*)$/; // intercepting route for one level up
-const INTERCEPTING_TWO_LEVEL_UP_RE = /^\(\.\.\)\(\.\.\)([a-z][a-z0-9-_]*)$/; // intercepting route for two level up
-const INTERCEPTING_ROOT_LEVEL_UP_RE = /^\(\.\.\.\)([a-z][a-z0-9-_]*)$/; // intercepting route for the root level up
+const NORMAL_RE = /^[a-zA-Z][a-zA-Z0-9_-]*$/; // normal route
+const GROUP_RE = /^\(([.]{3})?[a-zA-Z][a-zA-Z0-9-_]*\)$/;
+const DYNAMIC_RE = /^\[([a-zA-Z][a-zA-Z0-9_]*)\]$/; // dynamic route
+const CATCH_ALL_RE = /^\[\.{3}([a-zA-Z][a-zA-Z0-9_]*)\]$/; // catch-all route
+const OPTIONAL_CATCH_ALL_RE = /^\[\[\.{3}([a-zA-Z][a-zA-Z0-9_]*)\]\]$/; // optional catch-all route
+const PARALLEL_RE = /^@([a-zA-Z][a-zA-Z0-9-_]*)$/; // parallel route
+const INTERCEPTING_SAME_LEVEL_RE = /^\(\.\)([a-zA-Z][a-zA-Z0-9-_]*)$/; // intercepting route for the same level
+const INTERCEPTING_ONE_LEVEL_UP_RE = /^\(\.\.\)([a-zA-Z][a-zA-Z0-9-_]*)$/; // intercepting route for one level up
+const INTERCEPTING_TWO_LEVEL_UP_RE = /^\(\.\.\)\(\.\.\)([a-zA-Z][a-zA-Z0-9-_]*)$/; // intercepting route for two level up
+const INTERCEPTING_ROOT_LEVEL_UP_RE = /^\(\.\.\.\)([a-zA-Z][a-zA-Z0-9-_]*)$/; // intercepting route for the root level up
 
 module.exports = {
   PRESET_ROOT_LAYOUT,
